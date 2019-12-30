60f97baa486bbf4df059b9a20db68e5bcf139bd0
TJ Gore <hello@tjwgore.com>
2019-12-30 01:32:20 -0500

    clean up code

--- app/Http/Middleware/JWTAuthenticate.php
+++ app/Http/Middleware/JWTAuthenticate.php
@@ -22,11 +22,10 @@ class JWTAuthenticate extends Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if($guard == 'admin') {
-            auth()->shouldUse('admin');
-            auth()->authenticate($request);
-        } else {
-            $this->authenticate($request);
+            auth()->shouldUse('admin');    
         }
+
+        auth()->authenticate($request);
 
         return $next($request);
     }
